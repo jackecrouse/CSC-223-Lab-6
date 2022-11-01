0247ef3279182f77944202a0961bdf38118a37a1
Icho3159 <105511673+Icho3159@users.noreply.github.com>
2022-11-01 19:56:45 -0400

    PointNamingFactoryTests finished and working
    .

--- test/geometry_objects/points/PointNamingFactoryTest.java
+++ test/geometry_objects/points/PointNamingFactoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class PointNamingFactoryTest {
 
@@ -19,13 +20,16 @@ public class PointNamingFactoryTest {
 		Point p3 = new Point(2,0);
 		Point p4 = new Point(1000,1000);
 		
-		arr.add(p1); arr.add(p2); arr.add(p3); arr.add(p4);
+		arr.add(p1); 
+		arr.add(p2); 
+		arr.add(p3); 
+		arr.add(p4);
 		
 		return arr;
 	}
 	
 	@Test
-	public void putTest() {
+	public void putTestPoint() {
 		Point contained = new Point(0,0);
 		Point uncontained = new Point(3,3);
 		Point uncontained2 = new Point(5,3);
@@ -33,8 +37,112 @@ public class PointNamingFactoryTest {
 		PointNamingFactory empty = new PointNamingFactory();
 		
 		assertEquals(empty.put(uncontained), uncontained);
+		assertEquals(empty.put(uncontained2), uncontained2);
+		assertEquals(full.put(uncontained), uncontained);
+		assertEquals(full.put(contained), contained);
+	}
+	@Test
+	public void putTestXY() {
+		Point contained = new Point(0,0);
+		PointNamingFactory full = new PointNamingFactory(makePointList());
+		PointNamingFactory empty = new PointNamingFactory();
+		
+		Point p = full.put(0, 0);
+		assertEquals(p.compareTo(contained), 0);
+		
+		full.put(2,6);
+		assertEquals(full.size(), 6);
+		
+		empty.put(0,5);
+		assertEquals(empty.size(), 1);
+	}
+	@Test
+	public void putTestString() {
+		
+		PointNamingFactory full = new PointNamingFactory(makePointList());
+		PointNamingFactory empty = new PointNamingFactory();
+		
+		full.put("name", 7, 0);
+		assertEquals(full.size(), 5);
+		
+		full.put("name", 0, 0);
+		assertEquals(full.size(), 6);
+		
+		empty.put("name", 0, 0);
+		assertEquals(empty.size(), 1);
+		
+		empty.put(null, 1, 1);
+		assertEquals(empty.size(), 2);
+		
+	}
+	@Test
+	public void containsPointTest() {
+		PointNamingFactory pnf = new PointNamingFactory();
+		Point a = new Point(3,3);
+		Point b = new Point(0,0);
+		Point c = new Point(-100,3);
+		Point d = new Point(48,3);
+		
+		pnf.put(a);
+		pnf.put(b);
+		pnf.put(c);
+		
+		assertTrue(pnf.contains(a));
+		assertTrue(pnf.contains(b));
+		assertTrue(pnf.contains(c));
+		assertFalse(pnf.contains(d));
+	}
+	@Test
+	public void containsXYTest() {
+		PointNamingFactory pnf = new PointNamingFactory();
+		Point a = new Point(3,3);
+		Point b = new Point(0,0);
+		Point c = new Point(-100,3);
+		
+		pnf.put(a);
+		pnf.put(b);
+		pnf.put(c);
+		
+		assertTrue(pnf.contains(3, 3));
+		assertTrue(pnf.contains(0, 0));
+		assertTrue(pnf.contains(-100, 3));
+		assertFalse(pnf.contains(47, 26));
 	}
 	
+	@Test
+	public void getAllPointsTest() {
+		PointNamingFactory pnf = new PointNamingFactory();
+		Point a = new Point(3,3);
+		Point b = new Point(0,0);
+		Point c = new Point(-100,3);
+		
+		pnf.put(a);
+		pnf.put(b);
+		pnf.put(c);
+		
+		Object[] arr = pnf.getAllPoints().toArray();	
+
+		assertEquals(arr[0], a);
+		
+		assertEquals(arr[1], b);
+		
+		assertEquals(arr[2], c);
+	
+		pnf.put(a);
+	}
+	@Test
+	public void toSrintTest() {
+		PointNamingFactory pnf = new PointNamingFactory();
+
+		assertEquals(pnf.toString(), "[]");
+
+		pnf.put("a", 0, 0);
+		pnf.put("b", 10, 10);
+		pnf.put("c", -100, -100);
+		
+		assertEquals(pnf.toString(), "[(a : 0.0, 0.0)(b : 10.0, 10.0)(c : -100.0, -100.0)]");
+		
+		}
 	
 }
 
